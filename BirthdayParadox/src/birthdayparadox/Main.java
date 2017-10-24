177fe52ef19a20f4d50fda25d8bd007d7cf560d3
Emrullah Koçmar <emokocmar@gmail.com>
2017-10-24 19:05:39 +0300

    variable name changed

--- BirthdayParadox/src/birthdayparadox/Main.java
+++ BirthdayParadox/src/birthdayparadox/Main.java
@@ -5,12 +5,12 @@ public class Main
 
     public static void main(String[] args) throws Exception
     {
-        BirthdayGenerator bdayGen50 = new BirthdayGenerator(1000);
-        bdayGen50.generateRandomBdays();
+        BirthdayGenerator bdayGen = new BirthdayGenerator(1000);
+        bdayGen.generateRandomBdays();
 
         BirthdayParadox bdayParadox = new BirthdayParadox();
 
-        bdayParadox.calculateBdayParadox(bdayGen50.getBirthdays());
+        bdayParadox.calculateBdayParadox(bdayGen.getBirthdays());
         // bdayParadox.printBirthdayCollision();
         bdayParadox.printBdayCollasMatrix();
         
